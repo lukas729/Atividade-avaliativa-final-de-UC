fd62e373ff27bf4f1c434ffa56feadb886b56699
lukas729 <lukas177585@gmail.com>
2025-11-26 16:33:45 -0300

    ex18

---
+++ ex16.js
@@ -0,0 +1,9 @@
+for (let i = 5; i <= 10; i++) {
+    console.log(`Tabuada do ${i}:`);
+    
+    for (let j = 1; j <= 10; j++) {
+        console.log(`${i} x ${j} = ${i * j}`);
+    }
+    
+    console.log('----------------------');
+}
---
+++ ex18.js
@@ -0,0 +1,5 @@
+const array = [1, 2, 3, 4];
+console.log('Array original:', array);
+
+const arrayInvertido = array.reverse();
+console.log('Array invertido:', arrayInvertido);
